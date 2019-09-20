148d2f4af7bb5edf367915b8c6e34c18ed2a2bff
Bernard Thompson <bethomps@redhat.com>
2019-09-20 15:05:45 +0100

    [Lab01] Added placemarks to arraylist

---
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -0,0 +1,37 @@
+package org.wit.placemark.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_placemark.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
+import org.wit.placemark.R
+import org.wit.placemark.models.PlacemarkModel
+
+
+class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
+
+    var placemark = PlacemarkModel()
+    val placemarks = ArrayList<PlacemarkModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_placemark)
+        info("Placemark Activity started..")
+
+        btnAdd.setOnClickListener() {
+            placemark.title = placemarkTitle.text.toString()
+            if (placemark.title.isNotEmpty()) {
+                placemarks.add(placemark)
+                info("add Button Pressed: $placemark")
+                info("Placemarks: $placemarks")
+
+            }
+            else {
+                toast ("Please Enter a title")
+            }
+        }
+    }
+}
+
