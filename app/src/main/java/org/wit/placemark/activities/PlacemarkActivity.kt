1b30a7891ccb1ad06f2f61458d9f76a0afcd0b94
Bernard Thompson <bethomps@redhat.com>
2019-10-04 17:04:44 +0100

    [Lab02] Added main class

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -7,29 +7,34 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.placemark.R
+import org.wit.placemark.main.MainApp
 import org.wit.placemark.models.PlacemarkModel
 
 
 class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
 
     var placemark = PlacemarkModel()
-    val placemarks = ArrayList<PlacemarkModel>()
+    lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_placemark)
-        info("Placemark Activity started..")
+        app = application as MainApp
 
         btnAdd.setOnClickListener() {
+
             placemark.title = placemarkTitle.text.toString()
             placemark.desc = placemarkDesc.text.toString()
-            if (placemark.title.isNotEmpty()) {
-                placemarks.add(placemark)
-                info("Placemarks: $placemarks")
 
+            if (placemark.title.isNotEmpty() && placemark.desc.isNotEmpty()) {
+                app.placemarks.add(placemark.copy())
+                for (i in app.placemarks.indices) {
+                    info("Placemark[$i]:${this.app.placemarks[i]}")
+                }
             }
             else {
-                toast ("Please Enter a title")
+                toast ("Please Enter a title and description")
             }
         }
     }
